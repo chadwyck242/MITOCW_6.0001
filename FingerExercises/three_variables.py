8fa9a270571bd97206919d31f1816169cf06b543
chadwyck242 <chadwyck.codeart@gmail.com>
2020-05-25 18:02:14 -0600

    Add changes due to Python interpreter change

--- FingerExercises/three_variables.py
+++ FingerExercises/three_variables.py
@@ -1,1 +1,51 @@
-"""Write a program that examines three variables - x, y and z - and prints the largest odd number among them. If none of them are odd, it should print a message to that effect."""# Initialize variablesx = 2y = 4z = 1largest = 0# Branching logic to test for largest odd numberif (x % 2 or y % 2 or z % 2) != 0:    if (x % 2 == 0 and y % 2 == 0):        largest = z    elif (x % 2 == 0 and z % 2 == 0):        largest = y    elif (y % 2 == 0 and z % 2 == 0):        largest = x        if (x % 2 != 0 and y % 2 != 0 and z % 2 != 0):        if x > y and x > z:            largest = x        elif y > x and y > z:            largest = y        else:            largest = z        if (x % 2 != 0 and y % 2 != 0  and z % 2 == 0):        if x > y:            largest = x        else:            largest = y    elif(x % 2 != 0 and y % 2 == 0 and z % 2 != 0):        if x > z:            largest = x        else:            largest = z    elif(x % 2 == 0 and y % 2 != 0 and z % 2 != 0):        if y > z:            largest = y        else:            largest = z                print(largest)# Print message if all numbers even and nothing oddelse:    print("All numbers are even!")
+"""
+
+Write a program that examines three variables - x, y and z - and prints 
+the largest odd number among them. If none of them are odd, it should 
+print a message to that effect.
+
+"""
+
+# Initialize variables
+x = 2
+y = 4
+z = 1
+largest = 0
+
+# Branching logic to test for largest odd number
+if (x % 2 or y % 2 or z % 2) != 0:
+    if (x % 2 == 0 and y % 2 == 0):
+        largest = z
+    elif (x % 2 == 0 and z % 2 == 0):
+        largest = y
+    elif (y % 2 == 0 and z % 2 == 0):
+        largest = x
+
+    if (x % 2 != 0 and y % 2 != 0 and z % 2 != 0):
+        if x > y and x > z:
+            largest = x
+        elif y > x and y > z:
+            largest = y
+        else:
+            largest = z
+
+    if (x % 2 != 0 and y % 2 != 0 and z % 2 == 0):
+        if x > y:
+            largest = x
+        else:
+            largest = y
+    elif(x % 2 != 0 and y % 2 == 0 and z % 2 != 0):
+        if x > z:
+            largest = x
+        else:
+            largest = z
+    elif(x % 2 == 0 and y % 2 != 0 and z % 2 != 0):
+        if y > z:
+            largest = y
+        else:
+            largest = z
+
+    print(largest)
+# Print message if all numbers even and nothing odd
+else:
+    print("All numbers are even!")
