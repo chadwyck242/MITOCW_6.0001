93775b1eec91054d1bc225d32506222e4e1a8014
chadwyck242 <chadwyck.codeart@gmail.com>
2020-06-06 15:24:14 -0600

    Add completed PSET 0

---
+++ PSETS/ps0.py
@@ -0,0 +1,14 @@
+####################################
+# MIT OpenCourseWare 
+# 6.0001 Introduction to Computer Science
+# and Programming in Python
+# Problem Set PS0
+####################################
+
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("x**y = ", x**y)
+print("log(x)", math.log(x)) 
