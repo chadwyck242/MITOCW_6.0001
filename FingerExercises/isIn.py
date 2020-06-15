b96107577d2bca2946a3ad98ebc31dc4747172ec
chadwyck242 <chadwyck.codeart@gmail.com>
2020-06-14 20:02:43 -0600

    Add completed finger exercise from page 42 of textbook: function isIn

---
+++ FingerExercises/isIn.py
@@ -0,0 +1,16 @@
+#######################################################################################
+# Finger exercise from page 42 of the textbook: Write a function isIn that accepts two
+# strings as arguments and returns True if either string occurs anywhere in the other,
+# and False otherwise. Hint: You might want to use the built-in str operation in
+#######################################################################################
+
+
+def isIn(string1, string2):
+    if (string1 in string2) or (string2 in string1):
+        return True
+    else:
+        return False
+
+
+print(isIn('have', 'I have a pen'))
+print(isIn('says', 'Cake is good'))
